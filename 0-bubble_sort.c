53bf45240f5c58ca0f12d244f09436391fbc1942
Vishay Singh <vishay1998@gmail.com>
2023-06-22 05:02:25 -0700

    task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,34 @@
+#include "sort.h"
+
+/*
+ * bubble_sort - sorts an array using bubble sort algorithm
+ * @array: array to sort
+ * @size: size of array
+ * Return: no return
+ **/
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t inner, outer;
+	int temp, flag;
+
+	for (outer = size - 1; outer > 0; outer--)
+	{
+		flag = 0;
+		for (inner = 0; inner < outer; inner++)
+		{
+			if (array[inner] > array[inner + 1])
+			{
+				temp = array[inner];
+				array[inner] = array[inner + 1];
+				array[inner + 1] = temp;
+				flag = 1;
+				print_array(array, size);
+			}
+		}
+		if (!flag)
+		{
+			break;
+		}
+	}
+}
